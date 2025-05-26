997b6edac81f439de1c1c792b70d945826af9385
tobias-freire <tobiasfreire005@gmail.com>
2025-05-25 22:28:36 -0300

    fix: sentiment analysis service creates the kafka topic on initialization automatically, so there is no need to stop this service, create the topic manually and then run the service again anymore

--- sentiment-analysis-service/app.py
+++ sentiment-analysis-service/app.py
@@ -3,9 +3,24 @@ from contextlib import asynccontextmanager
 from threading import Thread
 from src.routes import router
 from src.consumer import consume_messages
+from src.configuration import general_conf
+from confluent_kafka.admin import AdminClient, NewTopic
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # Create Kafka topic
+    admin_client = AdminClient(general_conf)
+    topic_name = "feedbacks"
+    topic = NewTopic(topic_name, num_partitions=1, replication_factor=1)
+    result_dict = admin_client.create_topics([topic])
+    for topic, result in result_dict.items():
+        try:
+            result.result()
+            print(f"Topic {topic} created")
+        except Exception as e:
+            print(f"Failed to create topic {topic}: {e}")
+    
+    # Start the consumer thread
     consumer_thread = Thread(target=consume_messages, daemon=True)
     consumer_thread.start()
     yield
