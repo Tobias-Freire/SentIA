a9ff6b8cda6f9af91993465b5996a275e8acfe18
tobias-freire <tobiasfreire005@gmail.com>
2025-05-26 15:24:44 -0300

    config: add consumer configuration

--- sentia-frontend/src/utils/configs.js
+++ sentia-frontend/src/utils/configs.js
@@ -4,4 +4,11 @@ const producer_config = {
     'dr_msg_cb': true
 }
 
+const consumer_config = {
+    'bootstrap.servers': 'kafka:9093',
+    'group.id': 'sentia-group',
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': false
+}
+
 export default producer_config
