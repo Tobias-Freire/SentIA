07cb9901ac0738eec1a840bcde5372730ba5493d
tobias-freire <tobiasfreire005@gmail.com>
2025-05-28 13:23:56 -0300

    feat: add methods to list and count negative feedbacks based on date-time intervals in notification service

--- notification-service/src/main/java/com/sentia/notification_service/repository/FeedbackRepository.java
+++ notification-service/src/main/java/com/sentia/notification_service/repository/FeedbackRepository.java
@@ -4,6 +4,7 @@ import com.sentia.notification_service.model.FeedbackModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FeedbackRepository extends MongoRepository<FeedbackModel, String> {
@@ -14,4 +15,9 @@ public interface FeedbackRepository extends MongoRepository<FeedbackModel, Strin
     @Query(value = "{'general_classification': 'NEGATIVE'}", count = true)
     long countAllNegativeClassifications();
 
+    @Query(value = "{'general_classification': 'NEGATIVE', 'datetime': { $gte:  ?0, $lt:  ?1} }")
+    List<FeedbackModel> findAllNegativeClassificationsByDateRange(LocalDateTime start, LocalDateTime end);
+
+    @Query(value = "{'general_classification': 'NEGATIVE', 'datetime': { $gte:  ?0, $lt:  ?1} }", count = true)
+    long countNegativeClassificationsByDateRange(LocalDateTime start, LocalDateTime end);
 }
