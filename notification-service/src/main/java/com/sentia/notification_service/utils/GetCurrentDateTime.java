553fc0260b7a730a7f3f3cbe2c8d02ed6068a393
tobias-freire <tobiasfreire005@gmail.com>
2025-05-28 10:49:19 -0300

    feat: add getCurrentDateTime() utility to notification service to get current date and time in the format yyyy-MM-dd HH:mm:ss

---
+++ notification-service/src/main/java/com/sentia/notification_service/utils/GetCurrentDateTime.java
@@ -0,0 +1,12 @@
+package com.sentia.notification_service.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GetCurrentDateTime {
+    public String getCurrentDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
+    }
+}
