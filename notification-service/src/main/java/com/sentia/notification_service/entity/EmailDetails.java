8d8ad3a6eb8b1f60e692cd3519afc87824609e67
tobias-freire <tobiasfreire005@gmail.com>
2025-05-30 20:14:23 -0300

    feat: add method to send email to a specified receiver with subject and body

---
+++ notification-service/src/main/java/com/sentia/notification_service/entity/EmailDetails.java
@@ -0,0 +1,14 @@
+package com.sentia.notification_service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDetails {
+    private String recipient;
+    private String subject;
+    private String body;
+}
---
+++ notification-service/src/main/java/com/sentia/notification_service/service/EmailService.java
@@ -0,0 +1,34 @@
+package com.sentia.notification_service.service;
+
+import com.sentia.notification_service.entity.EmailDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public String sendEmail(EmailDetails emailDetails) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom(sender);
+            message.setTo(emailDetails.getRecipient());
+            message.setSubject(emailDetails.getSubject());
+            message.setText(emailDetails.getBody());
+
+            javaMailSender.send(message);
+            return "Email sent successfully!";
+        } catch (Exception e) {
+            return "Error sending email!" + e.getMessage();
+        }
+    }
+}
