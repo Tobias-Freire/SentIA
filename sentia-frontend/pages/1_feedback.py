a5e0aa1e255b08edd8994aeda6877509b44d98d4
tobias-freire <tobiasfreire005@gmail.com>
2025-06-04 14:16:24 -0300

    feat: add feedback input and submit logic to send the feedback to kafka topic

--- sentia-frontend/pages/1_feedback.py
+++ sentia-frontend/pages/1_feedback.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from src.kafka_producer import create_producer, send_message
+from src.config import topics
+
+producer = create_producer()
 
 st.write("""
 # Feedback Page
 """)
+st.text_input("Provide your feedback", key="feedback_input")
+if st.button("Submit Feedback"):
+    feedback = st.session_state.feedback_input
+    if feedback:
+        send_message(producer, topics[0], feedback)
+        st.success("Feedback submitted successfully!")
+    else:
+        st.error("Please enter some feedback before submitting.")
